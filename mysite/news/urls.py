ad1c2b290e22cb977d55fe59adf01eb8e2a85684
artem <artemfap@yandex.ru>
2018-05-09 23:27:22 +0300

    add app news

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from django.views.generic import ListView
 
+from .models import Articles
 from . import views
 
 urlpatterns = [
-    path('', views.news, name='news'),
+    path('', ListView.as_view(queryset=Articles.objects.all().order_by("-date")[:20]),
+
+         name='news'),
+    # path('', views.news, name='news'),
 ]
