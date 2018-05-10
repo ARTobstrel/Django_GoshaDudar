e333e328f624ff98a729a453d49c71d5ba1099cb
artem <artemfap@yandex.ru>
2018-05-10 23:47:35 +0300

    mysql

--- mysite/mainApp/views.py
+++ mysite/mainApp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def index(request):
     return render(request, 'mainApp/homePage.html')
+
 
 def contact(request):
     return render(request, 'mainApp/basic.html', {'values': ["Hello, please call me back later", "(099)0990809"]})
